636b1a2d404e868cd94d0b7314add6290064e2f6
Gautier Borot <g.borot@gmail.com>
2019-10-18 15:41:28 +0200

    added some show info on the profile page, and migration to add image, description and website to accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,7 +6,18 @@ class Account < ApplicationRecord
 
   has_many :posts
 
+  mount_uploader :photo, PhotoUploader
+
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def total_followers
+    0
+  end
+
+  def total_following
+    0
   end
 end
